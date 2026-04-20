603aa19fcf29ace45dc028d51087d770d4565742
ZubairWalker <zubairwalker@gmail.com>
2026-04-20 10:11:17 +0900

    feat: encrypted the environment variable

--- config/app.php
+++ config/app.php
@@ -97,7 +97,7 @@ return [
 
     'cipher' => 'AES-256-CBC',
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY', 'base64:y0mLCpht/XcMiAzAmChK6H/IKfp9uHWI9VhxPElUTOA='),
 
     'previous_keys' => [
         ...array_filter(
